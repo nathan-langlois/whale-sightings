859854ce43db78b91ff0c72b0d8bad930d9b70c6
Nathan Langlois <nplanglois@gmail.com>
2023-10-28 22:51:47 -0700

    create sighting model/migration

---
+++ app/Models/Sighting.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Sighting extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'when' => 'datetime',
+        'latitude' => 'double',
+        'longitude' => 'double',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type',
+        'when',
+        'latitude',
+        'longitude',
+        'notes',
+        'image_url',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_10_29_053218_create_sightings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sightings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('type');
+            $table->dateTime('when');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->longText('notes')->nullable();
+            $table->longText('image_url')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sightings');
+    }
+};
